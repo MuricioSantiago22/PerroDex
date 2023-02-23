ce6e7c1210cf8b12f9c00aa4c0380e4100696d14
mau <hishrcontact4@gmail.com>
2023-02-23 12:45:30 -0600

    Add changes
    add changes

--- app/src/main/java/com/example/perrodex/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/perrodex/ui/auth/LoginActivity.kt
@@ -2,15 +2,38 @@ package com.example.perrodex.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.navigation.findNavController
 import com.example.perrodex.R
 import com.example.perrodex.databinding.ActivityLoginBinding
+import com.example.perrodex.network.ApiResponseStatus
 
 class LoginActivity : AppCompatActivity(), LoginFragment.LoginFragmentActions, SignUpFragment.SignUpFragmentActions {
+
+    private val loginViewModel : LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        loginViewModel.status.observe(this){
+            status ->
+
+            when(status){
+                is ApiResponseStatus.Error -> {
+                    loadingWheel.visibility = View.GONE
+                    Toast.makeText(this, status.messageId,Toast.LENGTH_SHORT)
+
+                }
+                is ApiResponseStatus.Loading -> {
+                    loadingWheel.visibility = View.ç
+                }
+                is ApiResponseStatus.Success -> {}
+            }
+
+        }
     }
 
     override fun onRegisterButtonClick() {
@@ -23,6 +46,8 @@ class LoginActivity : AppCompatActivity(), LoginFragment.LoginFragmentActions, S
         password: String,
         passwordConfirmation: String
     ) {
+
+        loginViewModel.signUp(email, password, passwordConfirmation)
 
     }
 }
--- app/src/main/java/com/example/perrodex/ui/auth/LoginViewModel.kt
+++ app/src/main/java/com/example/perrodex/ui/auth/LoginViewModel.kt
@@ -1,10 +1,37 @@
 package com.example.perrodex.ui.auth
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.perrodex.model.User
+import com.example.perrodex.network.ApiResponseStatus
+import kotlinx.coroutines.launch
 
 class LoginViewModel: ViewModel() {
 
+    private val _user = MutableLiveData<User>()
+    val user: LiveData<User>
+    get() = _user
+
+    private val _status = MutableLiveData<ApiResponseStatus<User>>()
+    val status: LiveData<ApiResponseStatus<User>>
+    get() = _status
+
+    private val loginRepository = LoginRepository()
+
     fun signUp(email: String, password: String, passwordConfirmation: String) {
 
+        viewModelScope.launch {
+            _status.value = ApiResponseStatus.Loading()
+            handleResponseStatus(loginRepository.signUp(email, password, passwordConfirmation))
+        }
     }
+    private fun handleResponseStatus(apiResponseStatus: ApiResponseStatus<User>){
+        if (apiResponseStatus is ApiResponseStatus.Success){
+            _user.value = apiResponseStatus.data
+        }
+        _status.value = apiResponseStatus
+    }
+
 }
